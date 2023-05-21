a3519b3e391474ab9cbc5dd1703ccf5c28168f27
jaha-sudo <romanovajaha@gmail.com>
2023-05-22 02:55:26 +0500

    added  task39

---
+++ arrays/script39.js
@@ -0,0 +1,15 @@
+//code.mu.off/ru/javascript/book/prime/arrays/elements-getting.html
+// task1
+let someArr = [1, 2, 3];
+console.log(someArr[0], someArr[1], someArr[2]);
+
+// task2
+let arr = [1, 2, 3];
+console.log(arr[0], arr[1], arr[2]);
+
+// task3
+console.log(arr[0] + arr[1] + arr[2]);
+
+// task4
+arr = ["a", "b", "c", "d"];
+console.log(arr[0] + "+" + arr[1] + "+" + arr[2] + "+" + arr[3]);
