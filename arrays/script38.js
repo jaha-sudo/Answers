9e885ed1887e73eadffd1f943b815b83b98d090d
jaha-sudo <romanovajaha@gmail.com>
2023-05-22 02:44:09 +0500

    add task38

---
+++ arrays/script38.js
@@ -0,0 +1,17 @@
+// code.mu.off/ru/javascript/book/prime/arrays/intro.html
+
+// task1
+let someArr = [2, 43, 47, 54, 76];
+alert(someArr);
+console.log(someArr);
+
+// task2
+let anotherArr = [
+  "@angelinajolie",
+  "@johnnydepp",
+  "@adamlevine",
+  "@danreynolds",
+  "@mileycyrus",
+];
+alert(anotherArr)
+console.log(anotherArr);
