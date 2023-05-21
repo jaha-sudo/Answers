e44db95973a2b1239bfad67806b9ef90506ae291
jaha-sudo <romanovajaha@gmail.com>
2023-05-22 03:46:16 +0500

    added 48-49tasks

---
+++ objects/script48.js
@@ -0,0 +1,20 @@
+//code.mu.off/ru/javascript/book/prime/objects/intro.html
+
+// task1
+let obj = {
+  1: "monday",
+  2: "tuesday",
+  3: "wednesday",
+  4: "thursday",
+  5: "friday",
+  6: "saturday",
+  7: "sunday",
+};
+
+console.log(obj[1]);
+console.log(obj[2]);
+console.log(obj[3]);
+console.log(obj[4]);
+console.log(obj[5]);
+console.log(obj[6]);
+console.log(obj[7]);
---
+++ objects/script49.js
@@ -0,0 +1,20 @@
+//code.mu.off/ru/javascript/book/prime/objects/entire-object-display.html
+
+// task1
+let obj = {
+  1: "jan",
+  2: "feb",
+  3: "march",
+  4: "april",
+  5: "may",
+  6: "june",
+  7: "july",
+  8: "august",
+  9: "september",
+  10: "october",
+  11: "november",
+  12: "december",
+};
+
+console.log(obj);
+
