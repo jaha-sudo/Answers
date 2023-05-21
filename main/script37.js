84d69b3157a0f6f7b2ba3d65fd75d0f26f8938e5
jaha-sudo <romanovajaha@gmail.com>
2023-05-22 01:48:50 +0500

    add 37 task, moved 1-37 scripts to main folder

---
+++ main/script37.js
@@ -0,0 +1,34 @@
+// code.mu.off/ru/javascript/book/prime/basis/practicum/formulas.html
+
+// task1
+let rad = 3;
+const PI = 3.14;
+let sCircle = PI * rad ** 2;
+console.log(sCircle);
+
+// task2
+let aSquare = 34;
+let sSquare = aSquare ** 2;
+console.log(sSquare);
+
+// task3
+let aRectangle = 32;
+let bRectangle = 21;
+let sRectangle = aRectangle * bRectangle;
+console.log(sRectangle);
+
+// task4
+aRectangle = 76;
+bRectangle = 67;
+let pRectangle = (aRectangle + bRectangle) * 2;
+console.log(pRectangle);
+
+// task5 -  Цельсий х 1,8 + 32
+let tCelsius = 36.6;
+let tFahrenheit = tCelsius * 1.8 + 32;
+console.log(tFahrenheit);
+
+// task6 - (32 °F − 32) × 5/9
+tFahrenheit = 587;
+tCelsius = ((tFahrenheit - 32) * 5) / 9;
+console.log(tCelsius);
