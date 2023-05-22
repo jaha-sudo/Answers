f327686c66b1578530e3c1defb0fefdbcf0ed314
jaha-sudo <romanovajaha@gmail.com>
2023-05-22 16:02:44 +0500

    added task50

---
+++ objects/script50.js
@@ -0,0 +1,9 @@
+// code.mu.off/ru/javascript/book/prime/objects/string-keys.html
+
+// task1
+let user = {
+    firstname:'Johnie',
+    lastname:'Depp'
+}
+console.log(user['firstname']);
+console.log(user['lastname']);
